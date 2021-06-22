29d643af105aff705efea6beab366934228e2122
Don Chaney <dac5197@live.com>
2021-06-22 09:21:01 -0400

    Create Base and OrgModel Handlers
    Implement IHandler in BaseHandler abstract class and create OrgModelHandler inheriting from BaseHandler.

---
+++ DacmeOOM.Infrastructure/Handlers/BaseHandler.cs
@@ -0,0 +1,63 @@
+﻿using DacmeOOM.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DacmeOOM.Infrastructure.Handlers
+{
+    public abstract class BaseHandler<TEntity, TContext> : IHandler<TEntity>
+        where TEntity : class
+        where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public BaseHandler(TContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _context.Set<TEntity>().FindAsync(id);
+
+            if (entity is null)
+            {
+                return false;
+            }
+
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<List<TEntity>> GetAsync()
+        {
+            var entities = await _context.Set<TEntity>().ToListAsync();
+            return entities;
+        }
+
+        public async Task<TEntity> GetAsync(int id)
+        {
+            var entity = await _context.Set<TEntity>().FindAsync(id);
+            return entity;
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
---
+++ DacmeOOM.Infrastructure/Handlers/OrgModelHandler.cs
@@ -0,0 +1,20 @@
+﻿using DacmeOOM.Application.Models;
+using DacmeOOM.Infrastructure.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DacmeOOM.Infrastructure.Handlers
+{
+    public class OrgModelHandler : BaseHandler<OrgModel, ApplicationDbContext>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrgModelHandler(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
