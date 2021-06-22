163d49f77d1d6064ca6aad7ec471497c9cc1d845
Don Chaney <dac5197@live.com>
2021-06-22 09:20:07 -0400

    Add interface IHandler

---
+++ DacmeOOM.Application/Interfaces/IHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DacmeOOM.Application.Interfaces
+{
+    public interface IHandler<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> GetAsync();
+        Task<TEntity> GetAsync(int id);
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
