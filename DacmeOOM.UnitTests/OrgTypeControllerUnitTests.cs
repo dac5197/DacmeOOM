13049c889d02c07394a5970e23673d2fc89441dd
Don Chaney <dac5197@live.com>
2021-06-26 08:09:38 -0400

    Stub first unit test

---
+++ DacmeOOM.UnitTests/OrgTypeControllerUnitTests.cs
@@ -0,0 +1,24 @@
+using DacmeOOM.Core.Application.Queries.OrgTypeQueries;
+using MediatR;
+using Moq;
+using System;
+using Xunit;
+
+namespace DacmeOOM.UnitTests
+{
+    public class OrgTypeControllerUnitTests
+    {
+        [Fact]
+        public void GetId_WhenItemDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            var mediatorStub = new Mock<IMediator>();
+            //mediatorStub.Setup(m => m.Send(It.IsAny<GetOrgTypeByIdQuery.Query<int>>));
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
