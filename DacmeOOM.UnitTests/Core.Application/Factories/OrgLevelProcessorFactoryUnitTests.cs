e644e95e13fd4fd1ba9e733440c3bb648761be99
Don Chaney <dac5197@live.com>
2021-06-28 08:44:56 -0400

    Add unit tests for Processor Factory and OrgLevelProcessorFactory

---
+++ DacmeOOM.UnitTests/Core.Application/Factories/OrgLevelProcessorFactoryUnitTests.cs
@@ -0,0 +1,49 @@
+﻿using DacmeOOM.Core.Application.Factories;
+using DacmeOOM.Core.Application.Processors.OrgLevelProcessors;
+using DacmeOOM.Core.Domain.Interfaces;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DacmeOOM.UnitTests.Core.Application.Factories
+{
+    public class OrgLevelProcessorFactoryUnitTests
+    {
+        [Fact]
+        public void PropGetAll_WhenNew_ReturnsNewOrgLevelGetAllProcessor()
+        {
+            // Arrange
+            var serviceFactoryStub = new Mock<IServiceFactory>();
+
+            var sut = new OrgLevelProcessorFactory(serviceFactoryStub.Object);
+
+            // Act
+            var result = sut.GetAll;
+
+            // Assert
+            result.Should().NotBeNull()
+                .And.BeOfType<OrgLevelGetAllProcessor>();
+        }
+
+        [Fact]
+        public void PropGetById_WhenNew_ReturnsNewOrgLevelGetAllProcessor()
+        {
+            // Arrange
+            var serviceFactoryStub = new Mock<IServiceFactory>();
+
+            var sut = new OrgLevelProcessorFactory(serviceFactoryStub.Object);
+
+            // Act
+            var result = sut.GetById;
+
+            // Assert
+            result.Should().NotBeNull()
+                .And.BeOfType<OrgLevelGetByIdProcessor>();
+        }
+    }
+}
---
+++ DacmeOOM.UnitTests/Core.Application/Factories/ProcessorFactoryUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using DacmeOOM.Core.Application.Factories;
+using DacmeOOM.Core.Domain.Interfaces;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DacmeOOM.UnitTests.Core.Application.Factories
+{
+    public class ProcessorFactoryUnitTests
+    {
+        [Fact]
+        public void PropOrgLevel_WhenNew_ReturnsNewOrgLevelProcessorFactory()
+        {
+            // Arrange
+            var serviceFactoryStub = new Mock<IServiceFactory>();
+
+            var sut = new ProcessorFactory(serviceFactoryStub.Object);
+
+            // Act
+            var result = sut.OrgLevel;
+
+            // Assert
+            result.Should().NotBeNull()
+                .And.BeOfType<OrgLevelProcessorFactory>();
+        }
+    }
+}
