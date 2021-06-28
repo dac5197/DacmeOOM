838382e1f4505c11f02eab0627f0806ed82abc8f
Don Chaney <dac5197@live.com>
2021-06-28 15:22:59 -0400

    Add unit tests for OrgLevelController Post Endpoint

--- DacmeOOM.UnitTests/Web.Api/OrgTypeControllerUnitTests.cs
+++ DacmeOOM.UnitTests/Web.Api/OrgTypeControllerUnitTests.cs
@@ -1,8 +1,10 @@
 using AutoMapper;
 using DacmeOOM.Core.Application.Interfaces.IFactories;
+using DacmeOOM.Core.Application.Models;
 using DacmeOOM.Core.Application.Queries.OrgTypeQueries;
 using DacmeOOM.Core.Domain.Interfaces;
 using DacmeOOM.Core.Domain.Models;
+using DacmeOOM.Web.Api.Contracts.V1.RequestModels;
 using DacmeOOM.Web.Api.Contracts.V1.ResponseModels;
 using DacmeOOM.Web.Api.Controllers.V1;
 using DacmeOOM.Web.Api.Maps;
@@ -147,6 +149,114 @@ namespace DacmeOOM.UnitTests.Web.Api
             // Assert
             result
                 .Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task Post_WhenResultIsValid_ReturnsOkResult()
+        {
+            // Arrange
+            var entity = CreateRandomOrgLevelModel();
+
+            OrgLevelPostRequestModel postRequestModel = new()
+            {
+                Name = entity.Name,
+                Level = entity.Level,
+                OrgTypeId = entity.OrgTypeId
+            };
+
+            var response = CreateCommandResponseModel(entity, true);
+
+            var mapperMock = InjectMapper();
+
+            var processorFactoryStub = new Mock<IProcessorFactory>();
+            processorFactoryStub.Setup(x => x.OrgLevel.Add.ProcessAsync(It.IsAny<OrgLevelModel>())).ReturnsAsync(response);
+
+            var serviceFactoryStub = new Mock<IServiceFactory>();
+
+            var sut = new OrgLevelController(processorFactoryStub.Object, serviceFactoryStub.Object, mapperMock);
+
+            // Act
+            var result = await sut.Post(postRequestModel);
+            var objectResult = result as ObjectResult;
+
+            // Assert
+            objectResult
+               .Should().BeOfType<OkObjectResult>();
+
+            objectResult.StatusCode
+                .Should().Be(StatusCodes.Status200OK);
+
+            objectResult.Value
+                .Should().BeOfType<OrgLevelResponseModel>()
+                .And.Should().NotBeNull();
+
+            objectResult.Value
+                .Should().BeEquivalentTo(entity, options => options.ExcludingMissingMembers());
+        }
+
+        [Fact]
+        public async Task Post_WhenResultIsInvalid_ReturnsBadResult()
+        {
+            // Arrange
+            var entity = CreateRandomOrgLevelModel();
+
+            OrgLevelPostRequestModel postRequestModel = new()
+            {
+                Name = entity.Name,
+                Level = entity.Level,
+                OrgTypeId = entity.OrgTypeId
+            };
+
+            var response = CreateCommandResponseModel(entity, false);
+
+            var mapperMock = InjectMapper();
+
+            var processorFactoryStub = new Mock<IProcessorFactory>();
+            processorFactoryStub.Setup(x => x.OrgLevel.Add.ProcessAsync(It.IsAny<OrgLevelModel>())).ReturnsAsync(response);
+
+            var serviceFactoryStub = new Mock<IServiceFactory>();
+
+            var sut = new OrgLevelController(processorFactoryStub.Object, serviceFactoryStub.Object, mapperMock);
+
+            // Act
+            var result = await sut.Post(postRequestModel);
+            var objectResult = result as ObjectResult;
+
+            // Assert
+            objectResult
+               .Should().BeOfType<BadRequestObjectResult>();
+
+            objectResult.StatusCode
+                .Should().Be(StatusCodes.Status400BadRequest);
+
+            objectResult.Value
+                .Should().BeOfType<ErrorListReponseModel>()
+                .And.Should().NotBeNull();
+
+            objectResult.Value
+                .Should().BeEquivalentTo(response, options => options.ExcludingMissingMembers());
+        }
+
+        private static CommandResponseModel<OrgLevelModel> CreateCommandResponseModel(OrgLevelModel entity, bool isValid)
+        {
+            CommandResponseModel<OrgLevelModel> response = new() 
+            { 
+                Entity = entity,
+            };
+
+            if (isValid is false)
+            {
+                response.ErrorList = new();
+                response.ErrorList.Errors = new();
+                response.ErrorList.Errors.Add(
+                    new ErrorModel
+                    {
+                        PropertyName = "TestError",
+                        Message = "Test error message.  This is only a test"
+                    });
+            }
+
+            return response;
         }
 
         private OrgLevelModel CreateRandomOrgLevelModel()
